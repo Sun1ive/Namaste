9d2082d631a246eaac31709a04d75e9879a6614d
Sun1ive <sunliveua@gmail.com>
2017-08-09 16:04:35 +0300

    0.5 js fixes + some stuff

--- _scripts/common.js
+++ _scripts/common.js
@@ -2,14 +2,24 @@ $(document).ready(function() {
 	let windowHeight = $(document).height();
 	let speed = 3;
 	let timeScrolling = windowHeight / speed;
-	let topX = $('html').offset().top - 
+
+function checkPos(){
+	let pos = $(window).scrollTop();
+	if(pos < "700") {
+		$('.scrollTop').css('display', 'none');
+	}
+	else {
+		$('.scrollTop').css('display', 'block');
+	}
+}
+
+	$(document).scroll(function(e) {
+		checkPos();
+	})
+
 	$('.scrollTop').click(function(e) {
 		$('html, body').animate({
 			scrollTop: 0
 		}, timeScrolling);
 	});
-
-
-
-
 });
